78b09ea00fd5882391ac47f4f4293e24fb18d4de
Han Tuerker <han.tuerker@gmail.com>
2024-05-06 16:26:26 +0300

    init

---
+++ views/widget-button-container.php
@@ -0,0 +1,3 @@
+<div id="buildship-chat-widget-container" class="buildship-chat-widget-container">
+    <button data-buildship-chat-widget-button disabled><?php echo _e($button_name); ?></button>
+</div>
