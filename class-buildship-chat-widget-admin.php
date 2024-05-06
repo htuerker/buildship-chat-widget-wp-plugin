9e04590adbefb18936c1746aa20a2e177a3806f0
Han Tuerker <han.tuerker@gmail.com>
2024-05-06 16:30:59 +0300

    lint

--- class-buildship-chat-widget-admin.php
+++ class-buildship-chat-widget-admin.php
@@ -1,185 +1,186 @@
 <?php
-class ChatWidgetAdmin {
+class ChatWidgetAdmin
+{
 
-	public function add_hooks() {
-		add_action( 'admin_menu', array( $this, 'add_page' ) );
-		add_action( 'admin_init', array( $this, 'register_settings' ) );
-	}
-
-	public static function register_settings() {
-		$args = array(
-			'type'              => 'array',
-			'sanitize_callback' => array( ChatWidget::class, 'sanitize_settings' ),
-			'default'           => ChatWidget::settings_default(),
-		);
-		register_setting(
-			CHAT_WIDGET_SCREEN,
-			CHAT_WIDGET_SETTINGS,
-			$args
-		);
+  public function add_hooks()
+  {
+    add_action('admin_menu', array($this, 'add_page'));
+    add_action('admin_init', array($this, 'register_settings'));
   }
-	public function add_page() {
 
-		// Register settings and sections
-		$this->register_fields();
+  public static function register_settings()
+  {
+    $args = array(
+      'type' => 'array',
+      'sanitize_callback' => array(ChatWidget::class, 'sanitize_settings'),
+      'default' => ChatWidget::settings_default(),
+    );
+    register_setting(
+      CHAT_WIDGET_SCREEN,
+      CHAT_WIDGET_SETTINGS,
+      $args
+    );
+  }
+  public function add_page()
+  {
+    $this->register_fields();
 
-		// Add the page
-		$hook_suffix = add_options_page(
-			__( 'Buildship Chat Widget', 'chat-widget' ),
-			__( 'Settings', 'chat-widget' ),
-			'manage_options',
-			CHAT_WIDGET_SCREEN,
-			array( $this, 'render_page' )
-		);
+    $hook_suffix = add_options_page(
+      __('Buildship Chat Widget', 'chat-widget'),
+      __('Settings', 'chat-widget'),
+      'manage_options',
+      CHAT_WIDGET_SCREEN,
+      array($this, 'render_page')
+    );
 
-		// This adds a link in the plugins list table
-		add_action(
-			'plugin_action_links_' . plugin_basename( CHAT_WIDGET_MAIN_FILE ),
-			array(
-				$this,
-				'plugin_action_links_add_settings',
-			)
-		);
+    add_action(
+      'plugin_action_links_' . plugin_basename(CHAT_WIDGET_MAIN_FILE),
+      array(
+        $this,
+        'plugin_action_links_add_settings',
+      )
+    );
 
-		return $hook_suffix;
-	}
+    return $hook_suffix;
+  }
 
-	/**
-	 * All settings sections and fields.
-	 */
-	protected function sections() {
-		return array(
-			'basic-options' => array(
-				'name'     => __( 'Settings', 'chat-widget' ),
-				'settings' => array(
-					'endpoint_url' => array(
-						'label' => __( 'Endpoint URL', 'chat-widget' ),
-						'type'  => 'text',
-					),
+  protected function sections()
+  {
+    return array(
+      'basic-options' => array(
+        'name' => __('Settings', 'chat-widget'),
+        'settings' => array(
+          'endpoint_url' => array(
+            'label' => __('Endpoint URL', 'chat-widget'),
+            'type' => 'text',
+          ),
           'widget_title' => array(
-						'label' => __( 'Widget Title', 'chat-widget' ),
-						'type'  => 'text',
-					),
+            'label' => __('Widget Title', 'chat-widget'),
+            'type' => 'text',
+          ),
           'greeting_message' => array(
-						'label' => __( 'Greeting Message', 'chat-widget' ),
-						'type'  => 'text',
-					),
+            'label' => __('Greeting Message', 'chat-widget'),
+            'type' => 'text',
+          ),
           'button_name' => array(
-						'label' => __( 'Button Name', 'chat-widget' ),
-						'type'  => 'text',
-					),
-				)
-			),
+            'label' => __('Button Name', 'chat-widget'),
+            'type' => 'text',
+          ),
+        )
+      ),
       'advanced-options' => array(
-        'name'     => __( 'Advanced Options', 'chat-widget' ),
+        'name' => __('Advanced Options', 'chat-widget'),
         'settings' => array(
           'response_is_a_stream' => array(
-            'label' => __( 'Response is a Stream', 'chat-widget' ),
-            'type'  => 'checkbox',
+            'label' => __('Response is a Stream', 'chat-widget'),
+            'type' => 'checkbox',
           ),
           'close_on_outside_click' => array(
-            'label' => __( 'Close on Outside Click', 'chat-widget' ),
-            'type'  => 'checkbox',
+            'label' => __('Close on Outside Click', 'chat-widget'),
+            'type' => 'checkbox',
           ),
           'open_on_load' => array(
-            'label' => __( 'Open on Load', 'chat-widget' ),
-            'type'  => 'checkbox',
+            'label' => __('Open on Load', 'chat-widget'),
+            'type' => 'checkbox',
           ),
           'disable_error_alert' => array(
-            'label' => __( 'Disable Error Alert', 'chat-widget' ),
-            'type'  => 'checkbox',
+            'label' => __('Disable Error Alert', 'chat-widget'),
+            'type' => 'checkbox',
           ),
         ),
-			),
+      ),
 
-		);
-	}
+    );
+  }
 
-	public function register_fields() {
-		$sections = $this->sections();
-		$saved = ChatWidget::get_settings();
+  public function register_fields()
+  {
+    $sections = $this->sections();
+    $saved = ChatWidget::get_settings();
 
-		foreach ( $sections as $section_slug => $section_data ) {
-			add_settings_section(
-				$section_slug,
-				$section_data['name'],
-				null,
-				CHAT_WIDGET_SCREEN
-			);
-			foreach ( $section_data['settings'] as $id => $setting ) {
-				$value = isset( $saved[ $id ] ) ? $saved[ $id ] : null;
-				add_settings_field(
-					$id,
-					$setting['label'],
-					function()use ( $setting, $id, $value ) {
-						$this->render_field(
-							$setting['label'],
-							$id,
-							$value,
-							isset( $setting['required'] ) ? $setting['required'] : false,
+    foreach ($sections as $section_slug => $section_data) {
+      add_settings_section(
+        $section_slug,
+        $section_data['name'],
+        null,
+        CHAT_WIDGET_SCREEN
+      );
+      foreach ($section_data['settings'] as $id => $setting) {
+        $value = isset($saved[$id]) ? $saved[$id] : null;
+        add_settings_field(
+          $id,
+          $setting['label'],
+          function () use ($setting, $id, $value) {
+            $this->render_field(
+              $setting['label'],
+              $id,
+              $value,
+              isset ($setting['required']) ? $setting['required'] : false,
               $setting['type'],
-						);
-					},
-					CHAT_WIDGET_SCREEN,
-					$section_slug
-				);
-			}
-		}
+            );
+          },
+          CHAT_WIDGET_SCREEN,
+          $section_slug
+        );
+      }
+    }
 
-	}
+  }
 
-	public function render_field( $label, $name, $value = null, $required = false, $type = 'text' ) {
-		$name  = CHAT_WIDGET_SETTINGS . '[' . $name . ']';	
-		$field = sprintf(
-			'<label for="%s" %s>%s<input type="%s" name="%s" %s />%s</label>',
-			esc_attr( $name ),
-   			$type == 'checkbox' ? 'class="switch"' : '',
-			$type == 'checkbox' ? '' : esc_html( $label ),
-			esc_attr( $type ),
-			esc_attr( $name ),
-			sprintf(
-				' %s %s %s',
-				$required ? 'required' : '',
-		    $type == 'checkbox' && $value ? 'checked' : '',
-        $value ? 'value="' . esc_attr( $type == 'checkbox' ? 1 : $value ) . '"' : esc_attr( $type == 'checkbox' ? 0 : '' ) . '"',
-			),
-			$type == 'checkbox' ? '<div></div><span class="switch-bg"></span>' : '',
-		);
+  public function render_field($label, $name, $value = null, $required = false, $type = 'text')
+  {
+    $name = CHAT_WIDGET_SETTINGS . '[' . $name . ']';
+    $field = sprintf(
+      '<label for="%s" %s>%s<input type="%s" name="%s" %s />%s</label>',
+      esc_attr($name),
+      $type == 'checkbox' ? 'class="switch"' : '',
+      $type == 'checkbox' ? '' : esc_html($label),
+      esc_attr($type),
+      esc_attr($name),
+      sprintf(
+        ' %s %s %s',
+        $required ? 'required' : '',
+        $type == 'checkbox' && $value ? 'checked' : '',
+        $value ? 'value="' . esc_attr($type == 'checkbox' ? 1 : $value) . '"' : esc_attr($type == 'checkbox' ? 0 : '') . '"',
+      ),
+      $type == 'checkbox' ? '<div></div><span class="switch-bg"></span>' : '',
+    );
 
-		$wrapper = sprintf(
-			'<div class="field-wrapper">%s</div>',
-			$field
-		);
-		echo $wrapper;
-	}
+    $wrapper = sprintf(
+      '<div class="field-wrapper">%s</div>',
+      $field
+    );
+    echo $wrapper;
+  }
 
-	public function render_page() {
-		?>
-			<div class="buildship-chat-widget-wrap">
-        <div class="buildship-chat-widget-header">
-          <img src="<?php echo plugins_url("/assets/images/buildship.png", __FILE__); ?>" />
-				  <h1><?php esc_html_e( 'Buildship Chat Widget', 'buildship-chat-widget' ); ?></h1>
-        </div>
-				<form action="options.php" method="post" novalidate="novalidate">
-					<?php settings_fields( CHAT_WIDGET_SCREEN ); ?>
-					<?php do_settings_sections( CHAT_WIDGET_SCREEN ); ?>
-					<?php submit_button(); ?>
-				</form>
-			</div>
-		<?php
-	}
+  public function render_page()
+  {
+    ?>
+    <div class="buildship-chat-widget-wrap">
+      <div class="buildship-chat-widget-header">
+        <img src="<?php echo plugins_url("/assets/images/buildship.png", __FILE__); ?>" />
+        <h1><?php esc_html_e('Buildship Chat Widget', 'buildship-chat-widget'); ?></h1>
+      </div>
+      <form action="options.php" method="post" novalidate="novalidate">
+        <?php settings_fields(CHAT_WIDGET_SCREEN); ?>
+        <?php do_settings_sections(CHAT_WIDGET_SCREEN); ?>
+        <?php submit_button(); ?>
+      </form>
+    </div>
+    <?php
+  }
 
-	function plugin_action_links_add_settings( $links ) {
-		// Add link as the first plugin action link.
-		$settings_link = sprintf(
-			'<a href="%s">%s</a>',
-			esc_url( add_query_arg( 'page', CHAT_WIDGET_SCREEN, admin_url( 'options-general.php' ) ) ),
-			esc_html__( 'Settings', 'buildship-chat-widget' )
-		);
-		array_unshift( $links, $settings_link );
+  function plugin_action_links_add_settings($links)
+  {
+    $settings_link = sprintf(
+      '<a href="%s">%s</a>',
+      esc_url(add_query_arg('page', CHAT_WIDGET_SCREEN, admin_url('options-general.php'))),
+      esc_html__('Settings', 'buildship-chat-widget')
+    );
+    array_unshift($links, $settings_link);
 
-		return $links;
-	}
+    return $links;
+  }
 
 
 }
--- class-buildship-chat-widget.php
+++ class-buildship-chat-widget.php
@@ -1,58 +1,65 @@
 <?php
-class ChatWidget {
-	public static function add_hooks() {
-		add_action( 'plugins_loaded', array( __CLASS__, 'load_textdomain' ) );
+class ChatWidget
+{
+  public static function add_hooks()
+  {
+    add_action('plugins_loaded', array(__CLASS__, 'load_textdomain'));
     wp_enqueue_style("style", plugins_url("/assets/style.css", __FILE__));
-	}
+  }
 
-	public static function load_textdomain() {
-		load_plugin_textdomain( 'buildship' );
-	}
+  public static function load_textdomain()
+  {
+    load_plugin_textdomain('buildship');
+  }
 
-	public static function sanitize_settings( $values ) {
-		if ( ! is_array( $values ) ) {
-			$values = array();
-		}
-		$data     = array();
-		$defaults = self::settings_default();
-		foreach ( $defaults as $key => $default ) {
-			if ( ! array_key_exists( $key, $values ) ) {
-				$data[ $key ] = $default;
-			} else {
-				$data[ $key ] = $values[ $key ];
-			}
-		}
+  public static function sanitize_settings($values)
+  {
+    if (!is_array($values)) {
+      $values = array();
+    }
+    $data = array();
+    $defaults = self::settings_default();
+    foreach ($defaults as $key => $default) {
+      if (!array_key_exists($key, $values)) {
+        $data[$key] = $default;
+      } else {
+        $data[$key] = $values[$key];
+      }
+    }
 
-		return $data;
-	}
+    return $data;
+  }
 
-	public static function settings_default() {
-		$defaults = array(
+  public static function settings_default()
+  {
+    $defaults = array(
       // basic settings
-			'endpoint_url' => '',
-			'widget_title' => 'Buildship Chat Widget',
-			'greeting_message' => 'Hello, how can I help you?',
-			'button_name' => 'Beep Boop',
+      'endpoint_url' => '',
+      'widget_title' => 'Buildship Chat Widget',
+      'greeting_message' => 'Hello, how can I help you?',
+      'button_name' => 'Beep Boop',
       // advanced settings
       "response_is_a_stream" => 0,
       "close_on_outside_click" => 0,
       "open_on_load" => 0,
       "disable_error_alert" => 0,
-		);
+    );
 
-		return $defaults;
-	}
+    return $defaults;
+  }
 
-	public static function get_settings() {
-		$settings = get_option( CHAT_WIDGET_SETTINGS, self::settings_default() );
+  public static function get_settings()
+  {
+    $settings = get_option(CHAT_WIDGET_SETTINGS, self::settings_default());
 
-		return apply_filters( 'chat_widget_settings', $settings );
-	}
+    return apply_filters('chat_widget_settings', $settings);
+  }
 
-	public static function save_settings( array $update ) {
-		$update = apply_filters( 'chat_widget_pre_save_settings', $update );
-    
-		return update_option( CHAT_WIDGET_SETTINGS, $update );
-	}
+  public static function save_settings(array $update)
+  {
+    $update = apply_filters('chat_widget_pre_save_settings', $update);
+
+    return update_option(CHAT_WIDGET_SETTINGS, $update);
+  }
 
 }
